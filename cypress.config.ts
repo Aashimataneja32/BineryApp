6bf9b0317824657f87d373e5b9609685c44e119d
Aashima123 <aashimataneja32@gmail.com>
2023-11-17 11:09:53 +0530

    options changed in report

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
